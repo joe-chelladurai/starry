3fdd75b05132ef842f79c606afd7558595ec3bed
Meshach <58997358+joe-chelladurai@users.noreply.github.com>
2022-10-27 08:26:53 -0600

    Added plot_boxplot

--- R/plot_boxplot.R
+++ R/plot_boxplot.R
@@ -1,24 +1,48 @@
-plot_boxplot_UI <- function(id,
-                            boxplot_xvar,
-                            boxplot_yvar,
-                            boxplot_theme,
-                            boxplot_group,
-                            boxplot_code,
-                            boxplot_shape,
-                            boxplot_size,
-                            boxplot_wraprow,
-                            boxplot_wrapcol,
-                            boxplot_rmv,
-                            boxplot_width,
-                            boxplot_height,
-                            boxplot_title,
-                            boxplot_subtitle,
-                            boxplot_xlab,
-                            boxplot_ylab,
-                            boxplot_caption,
-                            boxplot_showcode,
-                            boxplot_instantlocal,
-                            boxplot_run) {
+
+plot_boxplot <- function(data, xvar, yvar, theme, group, code, shape, size, row, column, width, height, title, subtitle, xlab, ylab, caption, show_code){
+
+  if (missing(xvar)) {xvar = ""} else {yvar = deparse(substitute(xvar))}
+  if (missing(yvar)) {yvar = ""} else {yvar = deparse(substitute(yvar))}
+  if (missing(shape)) {shape = ""} else {shape = deparse(substitute(shape))}
+  if (missing(group)) {group = ""} else {group = deparse(substitute(group))}
+  if (missing(size)) {size = ""} else {size = deparse(substitute(size))}
+  if (missing(row)) {row = ""} else {row = deparse(substitute(row))}
+  if (missing(column)) {column = ""} else {column = deparse(substitute(column))}
+  if (missing(height)) {height = NA}
+  if (missing(width)) {width = NA}
+  if (missing(title)) {title = ""}
+  if (missing(subtitle)) {subtitle = ""}
+  if (missing(caption)) {caption = ""}
+  if (missing(xlab)) {xlab = ""}
+  if (missing(ylab)) {ylab = ""}
+  if (missing(theme)) {theme = "theme_bw"} else {theme = deparse(substitute(theme))}
+  if (missing(code)) {code = ""}
+  if (missing(show_code)) {show_code = FALSE}
+
+
+
+
+
+
+plot_boxplot_ui <- function(id,
+                            data,
+                            boxplot_xvar = xvar,
+                            boxplot_yvar = yvar,
+                            boxplot_theme = theme,
+                            boxplot_group = group,
+                            boxplot_code = code,
+                            boxplot_shape = shape,
+                            boxplot_size = size,
+                            boxplot_wraprow = row,
+                            boxplot_wrapcol = column,
+                            boxplot_width = width,
+                            boxplot_height = height,
+                            boxplot_title = title,
+                            boxplot_subtitle = subtitle,
+                            boxplot_xlab = xlab,
+                            boxplot_ylab = ylab,
+                            boxplot_caption = caption,
+                            boxplot_showcode = show_code) {
   ns <- NS(id)
   tagList(
     div(
@@ -179,12 +203,7 @@ plot_boxplot_UI <- function(id,
         ),
         div(
           class = "result-view",
-          fluidRow(actionButton(NS(id, "boxplot_rmv"),
-            class = "child",
-            label = NULL,
-            icon = icon("fas fa-times"),
-            value = boxplot_rmv
-          )),
+
           fluidRow(plotOutput(NS(id, "boxplot_plot"), width = "auto", height = "auto")),
           fluidRow(verbatimTextOutput(NS(id, "boxplot_text")) %>%
             tagAppendAttributes(class = "codeoutput"))
@@ -194,7 +213,7 @@ plot_boxplot_UI <- function(id,
   )
 }
 
-plot_boxplot_SE <- function(id) {
+plot_boxplot_se <- function(id) {
   moduleServer(id, function(input, output, session) {
     req(data)
 
@@ -208,7 +227,7 @@ plot_boxplot_SE <- function(id) {
       updateSelectInput(
         session,
         "boxplot_yvar",
-        choices = c(" ", names(data))
+        choices = c("", names(data))
       )
 
       updateSelectInput(
@@ -343,17 +362,6 @@ plot_boxplot_SE <- function(id) {
     ns <- NS(id)
 
 
-
-    observeEvent(input$boxplot_rmv, {
-      print(paste0(id, "-boxplot_text"))
-      print(paste("this is input: ", input))
-
-      removeUI(
-        selector = paste0("#", ns("placeholder1"))
-      )
-    })
-
-
     observeEvent(input$toggle_plot_options,
       {
         toggle("boxplot_width")
@@ -413,7 +421,7 @@ plot_boxplot_SE <- function(id) {
         },
         if (input$boxplot_yvar != "" && input$boxplot_xvar != "") {
           paste0(", ", input$boxplot_yvar)
-        } else if (input$boxplot_yvar != " ") {
+        } else if (input$boxplot_yvar != "") {
           paste0("y = ", input$boxplot_yvar)
         } else {
 
@@ -607,3 +615,52 @@ plot_boxplot_SE <- function(id) {
     })
   })
 }
+
+
+  ui <- fluidPage(
+    shinyjs::useShinyjs(),
+    tags$head(
+      tags$style(
+        HTML('
+              .input-view .well { width: 350px; margin-left: -10px; }
+              .well  { background-color: #ffffff !important;}
+              .result-view { margin-left: 20px; width: 700px; }
+              .toggle-btnplay { visibility: visible; background: none; }
+              .cont2 .shiny-input-container:not(.shiny-input-container-inline) { width: auto; 	max-width: 100%; }
+              .cont3 { margin-left: 10px; visibility: hidden; }
+              .grid-container { display: flex; }
+              #code { white-space: pre; margin: 20px; }
+              .module-name {margin-top: 4px; font-style: italic;  width: 275px;}
+              .shiny-text-output {  border: none;  margin-top: 20px;}
+              .bootstrap-switch.bootstrap-switch-focused {	border-color: #d4d0d0;	outline: 0;	-webkit-box-shadow: none; box-shadow: none;}
+              .bootstrap-switch.bootstrap-switch-mini .bootstrap-switch-handle-off, .bootstrap-switch.bootstrap-switch-mini .bootstrap-switch-handle-on, .bootstrap-switch.bootstrap-switch-mini .bootstrap-switch-label {padding: 1px 5px;font-size: 12px;line-height: 1;}
+              .btn-play {padding: 0 !important;  margin-bottom: 10px;border: none;}
+              .btn-play:hover {color: #000000; background-color:  #ffffff;border-color:  #ffffff;}
+              .module-style { text-align: left; background-color: #faf9f7; border: 0; margin-bottom: 5px;}
+              .parent .row .col-sm-3 {max-width: 400px !important;min-width: 300px !important;}
+              .custom-scroll {max-height: 80vh;min-height: 30vh;overflow-y: auto;overflow-x: hidden;position: relative;scrollbar-width: thin;padding-right: 15px;}
+              .custom-scroll::-webkit-scrollbar {width: 4px;background: #faf9f7;}
+              .custom-scroll::-webkit-scrollbar-track {-webkit-border-radius: 2px;border-radius: 2px;}
+              .custom-scroll::-webkit-scrollbar-thumb {-webkit-border-radius: 2px;border-radius: 2px;background:  #C0C0C0;}
+
+
+             ')
+      )
+    ),
+    theme = bslib::bs_theme(),
+    plot_boxplot_ui("module", data)
+  )
+  server <- function(input, output, session) {
+    plot_boxplot_se("module")
+  }
+
+
+  shinyApp(ui, server)
+
+
+
+}
+
+
+plot_boxplot(mtcars)
+
